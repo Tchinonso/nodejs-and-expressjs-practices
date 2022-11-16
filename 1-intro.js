b87247fc8516c3c380c54359b68e36ddfee421b0
Tchinonso <lightersmercury@gmail.com>
2022-11-16 10:54:25 +0100

    code started

---
+++ 1-intro.js
@@ -0,0 +1,8 @@
+let number = 12
+if (number > 10) {
+	console.log('large number')
+} else {
+	console.log("small numbers")
+}
+
+console.log("i've been writing nodejs codes since😄")
--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-let express = require('express');
+//GLOBALS - NO WINDOW!!!
+
+// __dirname - path to current directory
+// __filename - filename
+// require - function to use modules(COMMONJS)
+// module - info about current module(FILE)
+// process - info about env where the program is being executed
